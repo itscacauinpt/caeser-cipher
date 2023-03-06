3038ff9bcc1a687a892d9cba1b595e8f8bf2d74f
cacau <anaclaudia.sza@outlook.com>
2023-03-05 22:02:08 -0300

    feat: implement function + minimal css

--- src/views/DecodeView.vue
+++ src/views/DecodeView.vue
@@ -1,34 +1,79 @@
 <script setup>
 import { onMounted, ref, watch, computed } from 'vue';
+import { decodeFunc } from '../utils/constants'
 
-const message = ref('')
+let message = ref('')
+let decodedMessage = ref('...')
 
-const funcaozinha = () => {
-  console.log(message.value)
+const splitWord = (letters) => {
+  let letterMatched = ''
+  let wordMatched = ''
+
+  for (let i = 0; i < letters.length; i++) {
+    letterMatched = decodeFunc(letters[i].toUpperCase())
+    wordMatched += letterMatched
+  }
+
+  decodedMessage = wordMatched
+  return decodedMessage
+}
+
+const getMessage = () => {
+  splitWord(message.value.split(''))
+
+  message.value = ''
 }
 </script>
 
 <template>
   <main>
-    <div class="decode">
-      <form @submit.prevent="funcaozinha">
-        <h2>decode message</h2>
-        <textarea v-model="message" placeholder="decode teh message"></textarea>
+    <section>
+      <div class="decode">
+      <form @submit.prevent="getMessage">
+        <textarea v-model="message" placeholder="decode the message"></textarea>
         <input
           type="submit"
           value="decode"
         />
       </form>
     </div>
+    </section>
+    <section>
+      <div class="plaintext">
+        <p class="textstyle">{{ decodedMessage }}</p>
+      </div>
+    </section>
   </main>
 </template>
 
 <style>
+.decode {
+  padding: 22px 24px;
+  border-radius: 8px;
+  background-color: var(--vt-c-bg-soft);
+  margin: 1em;
+}
+
+.textstyle {
+  white-space: pre-line;
+}
+
+.plaintext {
+  padding: 22px 24px;
+  border-radius: 8px;
+  background-color: var(--vt-c-bg-soft);
+  margin: 1em;
+}
+
 @media (min-width: 1024px) {
-  .decode {
-    min-height: 100vh;
+  main {
     display: flex;
-    align-items: center;
+    place-items: center;
+    text-align: left;
+    margin-left: -1rem;
+    font-size: 1rem;
+    padding: 1rem 0;
+    margin-top: 1rem;
   }
 }
 </style>
